68c5445d0a97c8a00b1f3ea6657150d84ac3b30b
Muhammad Tifak <moht20009@gmail.com>
2020-03-05 12:40:04 -0800

    Added fog vanta script to lesson thermodynamics lesson

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+VANTA.GLOBE({
+  el: "#lesson1",
+  mouseControls: true,
+  touchControls: true,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00
+})
 
+VANTA.FOG({
+  el: "#lesson4",
+  mouseControls: true,
+  touchControls: true,
+  minHeight: 200.00,
+  minWidth: 200.00
+})
